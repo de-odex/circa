afd5eb3fb77e96e2574aab69b26c7456fbe2e7cf
Justin Kyle Ramos <de-odex@users.noreply.github.com>
2019-11-01 16:15:47 +0800

    feat: implement helper functions for perfect curves

--- src/circa/curve.nim
+++ src/circa/curve.nim
@@ -1,4 +1,7 @@
-import units
+import units, utils
+
+import math, sequtils, strformat
+
 
 #[
   refer to https://osu.ppy.sh/help/wiki/osu!_File_Formats/Osu_(file_format) to understand part of this
@@ -18,3 +21,44 @@ type
   Curve* = object
     curves*: seq[CurvePortion]
     reqLength*: float
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+proc getCenter(a, b, c: Position): Position =
+  let squareVec: Position = newPos(2)
+
+  let
+    aSquared = (b - c).pow(squareVec).arr.sum
+    bSquared = (a - c).pow(squareVec).arr.sum
+    cSquared = (a - b).pow(squareVec).arr.sum
+
+  if any(@[aSquared, bSquared, cSquared], proc (x: float64): bool = return x ~= 0):
+    raise newException(ValueError, &"given points are collinear: {a}, {b}, {c}")
+
+  let
+    s = aSquared * (bSquared + cSquared - aSquared)
+    t = bSquared * (aSquared + cSquared - bSquared)
+    u = cSquared * (aSquared + bSquared - cSquared)
+    stuSum = s + t + u
+
+  if stuSum ~= 0:
+    raise newException(ValueError, &"given points are collinear: {a}, {b}, {c}")
+
+  (s * a + t * b + u * c) / stuSum
+
+proc getCenter(oa: openarray[Position]): Position = getCenter(oa[0], oa[1], oa[2])
+
+proc rotate(position, center: Position, radians: float): Position =
+  var (pX, pY) = (position.x, position.y)
+  var (cX, cY) = (center.x, center.y)
+
+  var xDist = pX - cX
+  var yDist = pY - cY
+
+  newPos(
+    (xDist * cos(radians) - yDist * sin(radians)) + cX,
+    (xDist * sin(radians) + yDist * cos(radians)) + cY,
+  )
+
+proc binCoeff(n, k: int): float =
+  n.fac / (k.fac * (n - k).fac)
